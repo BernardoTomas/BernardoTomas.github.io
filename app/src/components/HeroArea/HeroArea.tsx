1ed692ca2cc221fbf9fe58d4135d6b10cd4057eb
Bernardo Tomas <bernardotomas.c14@gmail.com>
2025-05-20 09:57:43 -0300

    CHORE: working on hero area -> Made first smoke animation overlay

--- app/src/components/HeroArea/HeroArea.tsx
+++ app/src/components/HeroArea/HeroArea.tsx
@@ -2,11 +2,19 @@ import { useContext } from "react";
 import { ThemeContext } from "../../context/toggle-theme";
 import ImageCard from "./ImageCard/ImageCard";
 import TextCard from "./TextCard/TextCard";
-import smokeOverlayBottom from "../../assets/smoke_overlay_bottom.png";
+import HeroOverlay from "./HeroOverlay/HeroOverlay";
 import "./HeroArea.css";
 
 function HeroArea() {
   const { theme } = useContext(ThemeContext);
+
+  const smokeAnimationDelayClasses = [
+    "smoke-animation",
+    "smoke-animation-offset-2s",
+    "smoke-animation-offset-4s",
+    "smoke-animation-offset-6s",
+  ];
+
   return (
     <div className={theme + " hero-container"}>
       <div className="hero-content">
@@ -14,20 +22,9 @@ function HeroArea() {
         <TextCard />
       </div>
       <div className="hero-overlay">
-        <div className="smoke-container-purple">
-          <img
-            className="smoke1"
-            src={smokeOverlayBottom}
-            alt="smoke effect overlay"
-          />
-          <div className="smoke-container-yellow">
-            <img
-              className="smoke2"
-              src={smokeOverlayBottom}
-              alt="smoke effect overlay"
-            />
-          </div>
-        </div>
+        {smokeAnimationDelayClasses.map((animClass) => (
+          <HeroOverlay smokeAnimation={animClass} />
+        ))}
       </div>
     </div>
   );
