16e2043c1dd0dc769cd9e04b2707073dd298a057
Bernardo Tomas <bernardotomas.c14@gmail.com>
2025-06-04 11:47:19 -0300

    REFACTOR: Optimized the theme toggle logic, FEAT: Started working on generic TextCard component

--- app/src/components/AboutMe/AboutMe.tsx
+++ app/src/components/AboutMe/AboutMe.tsx
@@ -1,10 +1,24 @@
 import { ThemeContext } from "../../context/toggle-theme";
 import { useContext } from "react";
 
+import TextCard from "../GenericCards/TextCard";
+
+import "./AboutMe.css";
+
 function AboutMe() {
   const { theme } = useContext(ThemeContext);
 
-  return <div className={theme + " about-me-container"}>About me</div>;
+  return (
+    <div className={theme + " about-me-wrapper"}>
+      <div className={"about-me-container"}>
+        <TextCard
+          title="About Me"
+          pText="I am a passionate Full Stack Web Developer and Designer with a keen interest in creating innovative solutions. My journey in web development has equipped me with a diverse skill set, enabling me to tackle complex challenges and deliver high-quality applications."
+          cardStyle="card-1"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default AboutMe;
---
+++ app/src/components/GenericCards/TextCard.tsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import { ThemeContext } from "../../context/toggle-theme";
+
+import "./TextCard.css";
+
+type TextCardProps = {
+  title?: string;
+  subtitle?: string;
+  pText?: string;
+  highlightedText?: string;
+  pTextSecondPart?: string;
+  cardStyle: string;
+  buttonContent?: string;
+  buttonStyle?: string;
+};
+
+function TextCard(props: TextCardProps) {
+  const { theme } = useContext(ThemeContext);
+  const { title, subtitle, pText, cardStyle } = props;
+
+  return (
+    <div className={cardStyle + " " + theme + " card-container"}>
+      {title ? <h2>{title}</h2> : ""}
+      {title ? <h3>{subtitle}</h3> : ""}
+      {pText ? <p>{pText}</p> : ""}
+    </div>
+  );
+}
+
+export default TextCard;
