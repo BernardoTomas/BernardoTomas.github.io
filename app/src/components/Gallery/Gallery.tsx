72ae38bd7926e1e42a21037a4182b91cc37aa1ab
Bernardo Tomas <bernardotomas.c14@gmail.com>
2026-07-16 21:32:53 -0300

    [CHORE]: made the My Stacks section in the Gallery section

--- app/src/components/Gallery/Gallery.tsx
+++ app/src/components/Gallery/Gallery.tsx
@@ -4,8 +4,15 @@ import { ThemeContext } from "../../context/toggle-theme";
 import "./Gallery.css";
 import GalleryCard from "../GenericCards/GalleryCard";
 import galleryData from "../../data/galleryData";
+import {
+  languagesStacks,
+  frontEndStacks,
+  backEndStacks,
+  serverStacks,
+  otherStacks,
+  designStacks,
+} from "../../data/stacksData";
 
-import tempStackImg from "../../assets/Icons/css.png";
 import StackCard from "../GenericCards/StackCard";
 
 function Gallery() {
@@ -23,8 +30,41 @@ function Gallery() {
         </div>
         <h2 className="gallery-title title-left">My Stacks</h2>
         <div className="gallery-title-separator gallery-separator-left" />
-        <div className="gallery-box">
-          <StackCard imgSrc={tempStackImg} title="CSS" />
+        <h3 className="gallery-subtitle">Languages</h3>
+        <div className="gallery-box stacks-gallery-box">
+          {languagesStacks.map((stack, index) => (
+            <StackCard key={index} imgSrc={stack.url} title={stack.name} />
+          ))}
+        </div>
+        <h3 className="gallery-subtitle">Frontend</h3>
+        <div className="gallery-box stacks-gallery-box">
+          {frontEndStacks.map((stack, index) => (
+            <StackCard key={index} imgSrc={stack.url} title={stack.name} />
+          ))}
+        </div>
+        <h3 className="gallery-subtitle">Backend</h3>
+        <div className="gallery-box stacks-gallery-box">
+          {backEndStacks.map((stack, index) => (
+            <StackCard key={index} imgSrc={stack.url} title={stack.name} />
+          ))}
+        </div>
+        <h3 className="gallery-subtitle">Server</h3>
+        <div className="gallery-box stacks-gallery-box">
+          {serverStacks.map((stack, index) => (
+            <StackCard key={index} imgSrc={stack.url} title={stack.name} />
+          ))}
+        </div>
+        <h3 className="gallery-subtitle">Design</h3>
+        <div className="gallery-box stacks-gallery-box">
+          {designStacks.map((stack, index) => (
+            <StackCard key={index} imgSrc={stack.url} title={stack.name} />
+          ))}
+        </div>
+        <h3 className="gallery-subtitle">Other Stacks</h3>
+        <div className="gallery-box stacks-gallery-box">
+          {otherStacks.map((stack, index) => (
+            <StackCard key={index} imgSrc={stack.url} title={stack.name} />
+          ))}
         </div>
       </div>
     </div>
